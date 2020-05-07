3520282d46f87ebc10b1e271c02fc31e8da3a1a5
sehera <55153575+sehera@users.noreply.github.com>
2020-05-07 17:46:08 -0400

    Add files via upload

---
+++ js/code.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+  $("#backgcolor").on("click", function (){
+  $("body").css("background-color", "#AE95C4")
+  });
+
+
+
+$( function() {
+$( "#draggable1" ).draggable();
+  });
+
+  $( function() {
+  $( "#draggable2" ).draggable();
+    });
+
+    $( function() {
+    $( "#draggable3" ).draggable();
+      });
+
+      $( function() {
+      $( "#draggable4" ).draggable();
+        });
+
+
+
+
+
+})
---
+++ script.js
@@ -0,0 +1,13 @@
+
+function changeImage() {
+
+        if (document.getElementById("imgClickAndChange").src == "https://res.cloudinary.com/daa5ehdxc/image/upload/v1588051528/final%20website/img1_brdh7b.png")
+
+        {
+            document.getElementById("imgClickAndChange").src = "https://res.cloudinary.com/daa5ehdxc/image/upload/v1588051528/final%20website/img1_brdh7b.png";
+        }
+        else
+        {
+            document.getElementById("imgClickAndChange").src = "https://res.cloudinary.com/daa5ehdxc/image/upload/v1588051527/final%20website/note1_wmnhoh.png";
+        }
+    }
